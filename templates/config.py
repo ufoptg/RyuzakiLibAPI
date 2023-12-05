12fe30fa6ee4a30c3a354831a8e2e41bc0ff6d76
xtsea <90479255+xtsea@users.noreply.github.com>
2023-12-05 17:33:03 +0700

    _

--- templates/config.py
+++ templates/config.py
@@ -3,4 +3,5 @@ DESCRIPTION="description template"
 VERSION="0.0.1"
 TERMS_OF_SERVICE="terms template"
 DOCS_URL="/"
-API_KEYS=""
+API_KEYS="" # get api key from @randydev_bot use /token_api_key
+TELEGRAM_ID=0
